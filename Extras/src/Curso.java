b37594b2f2587387f50a875ba306ca234b578ce3
milir99 <milagrosabril.r@gmail.com>
2024-03-29 19:34:29 -0300

    ejercicios extra

---
+++ Extras/src/Curso.java
@@ -0,0 +1,30 @@
+public class Curso {
+    private String nombreCurso;
+    private string [] estudiantes;
+
+    public Curso() {
+        this.nombreCurso =¨¨;
+        this.estudiantes = ¨¨;
+    }
+
+    public Curso(String nombreCursoP, string[] estudiantesP) {
+        this.nombreCurso = nombreCursoP;
+        this.estudiantes = estudiantesP;
+    }
+
+    public String getNombreCurso() {
+        return nombreCurso;
+    }
+
+    public void setNombreCurso(String nombreCursoP) {
+        this.nombreCurso = nombreCursoP;
+    }
+
+    public string[] getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(string[] estudiantesP) {
+        this.estudiantes = estudiantesP;
+    }
+}
---
+++ Extras/src/Estudiante.java
@@ -0,0 +1,68 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double[] calificaciones;
+    private int validos;
+
+
+    public Estudiante() {
+        this.nombre = "";
+        this.edad = 0;
+    }
+
+    public Estudiante(String nombre_p, int edad_p) {
+        this.nombre = nombre_p;
+        this.edad = edad_p;
+        this.calificaciones = new double[10];
+        this.validos = 0;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre_p) {
+        this.nombre = nombre_p;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad_p) {
+        this.edad = edad_p;
+    }
+
+    public double[] getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(double[] calificaciones_p) {
+        this.calificaciones = calificaciones_p;
+    }
+
+    public void agregarCalificacion (double nuevaCalificacion)
+    {
+        this.calificaciones[validos] = nuevaCalificacion;
+        validos++;
+    }
+    private double obtenerPromedio(double[]calificacionesP)
+    {
+        double suma=0;
+
+        for(int i=0; i<calificacionesP.length;i++) {
+suma= suma+calificacionesP[i];
+        }
+        double promedio= suma/calificacionesP.length;
+        return promedio;
+    }
+    private void imprimirInformacion (String nombre_p, int edad_p, double calificacionP[])
+    {
+        System.out.println("Nombre: "+ nombre_p);
+        System.out.println("Edad: "+edad_p);
+       double promedio= obtenerPromedio(calificacionP);
+        System.out.println("Promedio: "+ promedio);
+
+    }
+}
